c66416e1a1c00ad0c5307b54a4e978fa79aff1ae
Mark Howells-Mead <permanent.tourist@gmail.com>
2015-04-13 17:21:24 +0200

    Extend commenting

--- wp-content/themes/frp_wordpress_child/functions.php
+++ wp-content/themes/frp_wordpress_child/functions.php
@@ -1,4 +1,10 @@
 <?php
+/**
+* This file contains the function definitions for the Child Theme.
+* In order to maintain the App logic, place your own functions in Classes/Theme.php, not here. 
+* This file is loaded BEFORE the equivalent version in the Parent Theme.
+*/
+
 require_once(get_template_directory().'/Classes/App.php');
 
 $app = Frp\WordPress\App::getSingleton();
@@ -6,5 +12,5 @@ $app = Frp\WordPress\App::getSingleton();
 // load your own functions from the class file frp_wordpress_child/Classes/Theme.php
 $app->extend('theme', $app->key);
 
-// load additional parent functions from the class file frp_wordpress_parent/Classes/Youtube.php
+// load additional non-essential parent functions, e.g. from the class file frp_wordpress_parent/Classes/Youtube.php
 //$app->extend('youtube');
