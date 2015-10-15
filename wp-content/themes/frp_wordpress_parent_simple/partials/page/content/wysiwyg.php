bdfb3d4c1073eb44527fc60ad69fe67e24aaec62
Mark Howells-Mead <permanent.tourist@gmail.com>
2015-10-15 18:19:08 +0200

    Only output wrappers if WYSIWYG not empty

--- wp-content/themes/frp_wordpress_parent_simple/partials/page/content/wysiwyg.php
+++ wp-content/themes/frp_wordpress_parent_simple/partials/page/content/wysiwyg.php
@@ -1,27 +1,34 @@
+<?php
+
+	the_post();
+	$the_content = get_the_content();
+	
+	if( !empty($the_content) ){
+
+?>
 
 <section class="module row constrained wysiwyg">
 	
 	<div class="inner">
 
 		<?php
-			if ( have_posts() ){
-		
-				while ( have_posts() ){
-		
-					the_post();
-					the_content();
-		
-				}
-		
-				get_template_part( 'partials/page/pagination' );
-		
-			} else {
-		
-				get_template_part( 'partials/page/none' );
-		
-			}
+
+			echo $the_content;
+
+			get_template_part( 'partials/page/pagination' );
+			
 		?>
 
 	</div>
 
 </section>
+
+<?php
+
+	} else {
+
+		get_template_part( 'partials/page/none' );
+
+	}
+
+?>
